8a60c2ecba04382d3f52ed818058586df78cd8d5
Alexander Dalshov <dalshov@gmail.com>
2018-12-09 19:10:43 +0300

    minor

--- modules/private/ccls/config.el
+++ modules/private/ccls/config.el
@@ -4,6 +4,17 @@
   ;; :when (featurep! :private +lsp
   :hook ((c-mode c++-mode cuda-mode objc-mode) . '+setup-ccls)
   :init
+  ;; https://github.com/maskray/ccls/blob/master/src/config.h
+  ;; (setq ccls-extra-init-params
+  ;;  `(:clang (:extraArgs ["--gcc-toolchain=/usr"]
+  ;;            :pathMappings ,+ccls-path-mappings)
+  ;;           ;; :completion
+  ;;           ;; (:include
+  ;;           ;;  (:blacklist
+  ;;           ;;   ["^/usr/(local/)?include/c\\+\\+/[0-9\\.]+/(bits|tr1|tr2|profile|ext|debug)/"
+  ;;           ;;    "^/usr/(local/)?include/c\\+\\+/v1/"
+  ;;           ;;    ]))
+  ;;           :index (:initialBlacklist ,+ccls-initial-blacklist :trackDependency 1)))
   (setq ccls-initialization-options '(
         :index (:comments 2)
         :completion (:detailedLabel t)
@@ -24,18 +35,6 @@
     ;; Use https://github.com/emacs-mirror/emacs/commits/feature/noverlay
     (setq ccls-sem-highlight-method 'font-lock)
     (ccls-use-default-rainbow-sem-highlight))
-
-  ;; https://github.com/maskray/ccls/blob/master/src/config.h
-  ;; (setq ccls-extra-init-params
-  ;;  `(:clang (:extraArgs ["--gcc-toolchain=/usr"]
-  ;;            :pathMappings ,+ccls-path-mappings)
-  ;;           ;; :completion
-  ;;           ;; (:include
-  ;;           ;;  (:blacklist
-  ;;           ;;   ["^/usr/(local/)?include/c\\+\\+/[0-9\\.]+/(bits|tr1|tr2|profile|ext|debug)/"
-  ;;           ;;    "^/usr/(local/)?include/c\\+\\+/v1/"
-  ;;           ;;    ]))
-  ;;           :index (:initialBlacklist ,+ccls-initial-blacklist :trackDependency 1)))
 
   (evil-set-initial-state 'ccls-tree-mode 'emacs)
   (set-company-backend! '(c-mode c++-mode cuda-mode objc-mode) 'company-lsp)
