55e9a2d159211156acd65322681a18ab244e322f
Alexander Dalshov <adalshov@charlesworthresearch.com>
2020-01-18 00:28:13 +0300

    flycheck was moved to other section

--- init.el
+++ init.el
@@ -89,7 +89,6 @@
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       flycheck          ; tasing you for every semicolon you forget
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
